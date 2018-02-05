31d8a2943f7b8e2062ce05753725b8e7267c7099
maheshnadar <maheshnadar@wohlig.com>
2018-02-05 19:23:08 +0530

    lobby done and working on side menu

--- www/js/lobbyctrl.js
+++ www/js/lobbyctrl.js
@@ -1,3 +1,22 @@
-myApp.controller("LobbyCtrl",function($scope){
-    console.log("hi this is lobby")
-    });
+myApp.controller("LobbyCtrl", function ($scope) {
+  console.log("hi this is lobby")
+  $scope.VariationActive = 'no';
+  $scope.sideMenu = 'no';
+
+  $scope.variationToggle = function () {
+    if ($scope.VariationActive == 'variation_active') {
+      $scope.VariationActive = "no";
+    } else {
+      $scope.VariationActive = 'variation_active';
+    }
+  }
+
+  $scope.closeMenu = function () {
+$scope.sideMenu="no"
+  }
+  $scope.openMenu = function () {
+    $scope.sideMenu="menu_open"
+  }
+
+
+});
