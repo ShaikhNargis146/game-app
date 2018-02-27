3d22744fd3628d518355cee028f0c5e5496012c7
kothavaleganesh <ganesh.kothavale@wohlig.com>
2018-02-27 19:11:39 +0530

    poker

---
+++ www/js/Pokerctrl.js
@@ -0,0 +1,10 @@
+myApp.controller("PokerCtrl", function ($scope, Service, $state, $ionicPlatform) {
+    $ionicPlatform.ready(function () {
+      screen.orientation.lock('portrait')
+    })
+  
+  
+   
+  
+  });
+  
